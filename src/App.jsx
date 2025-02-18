a25123c8ce5bf7d571d52034e7ece1f1d677f9b8
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-02-18 23:31:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-center my-10 py-5 bg-green-300 text-black rounded-md font-sans text-3xl'>Hello world</h1>
+
+    </>
+  )
+}
+
+export default App
